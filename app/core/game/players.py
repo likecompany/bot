14886164d4d7e046c07a8df36a240b03404d3ca7
toymaj <sermed512@gmail.com>
2023-10-19 20:37:19 +0300

    fix erros

--- app/core/game/players.py
+++ app/core/game/players.py
@@ -4,12 +4,25 @@ from likeinterface import Interface
 from likeinterface.methods import GetUser
 
 from schemas import Player, Session
+from utils.find_if import find_if
 
 
 async def update_players(
     interface: Interface,
     session: Session,
 ) -> None:
+    for position, player in enumerate(session.game.players):
+        if not find_if(
+            collection=session.players, condition=lambda element: element.id == player.id
+        ):
+            session.players.append(
+                Player(
+                    position=position,
+                    user=await interface.request(method=GetUser(user_id=player.id)),
+                    **player.model_dump(),
+                )
+            )
+
     session.players = [
         Player(
             position=position,
