2729291741d9e30ebfe8dc4c9b4cdda439885220
toymaj <sermed512@gmail.com>
2023-10-19 20:22:22 +0300

    fix keyboards

--- app/keyboards/game.py
+++ app/keyboards/game.py
@@ -66,13 +66,12 @@ def game_inline_keyboard_builder(redis_callback_data_key: str) -> InlineKeyboard
 def game_ended_inline_keyboard_builder(redis_callback_data_key: str) -> InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
 
-    builder.from_markup(
-        game_inline_keyboard_builder(redis_callback_data_key=redis_callback_data_key)
-        .attach(
+    builder.attach(
+        game_inline_keyboard_builder(redis_callback_data_key=redis_callback_data_key).attach(
             players_game_inline_keyboard_builder(redis_callback_data_key=redis_callback_data_key)
         )
-        .as_markup()
     )
+
     builder.row(
         InlineKeyboardButton(
             text="Winners",
